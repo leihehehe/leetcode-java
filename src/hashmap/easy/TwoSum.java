ba18858ea8bb89965b51cfc5b84618ac6106f07d
LeiH <hslscarlett@gmail.com>
2022-05-05 19:56:18 +1000

    test

---
+++ src/hashmap/easy/TwoSum.java
@@ -0,0 +1,13 @@
+package hashmap.easy;
+
+/***
+ * 1. TwoSum
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+
+    }
+}
