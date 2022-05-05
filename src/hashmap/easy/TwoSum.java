10e30c0b793ae99acd81b02c03ae2aa419b32832
LeiH <hslscarlett@gmail.com>
2022-05-05 20:06:42 +1000

    two sum

--- src/hashmap/easy/TwoSum.java
+++ src/hashmap/easy/TwoSum.java
@@ -1,13 +1,24 @@
 package hashmap.easy;
 
+import java.util.HashMap;
+
 /***
  * 1. TwoSum
  */
 public class TwoSum {
-    public static void main(String[] args) {
 
-    }
     public int[] twoSum(int[] nums, int target) {
-
+        HashMap storeMap = new HashMap();
+        //[2,5,7,3] => 9 => [0,2]
+        for(int i=0;i<nums.length;i++){
+            int need = target-nums[i];
+            Object value = storeMap.get(need);
+            if(value!=null){
+                return new int[]{(int)value,i};
+            }else{
+                storeMap.put(nums[i],i);
+            }
+        }
+        return new int[0];
     }
 }
