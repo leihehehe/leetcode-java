d322d46aaa599adb2d05da80f06533e998e1cf37
LeiH <hslscarlett@gmail.com>
2022-05-14 23:55:05 +1000

    Combination sum

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import backtracking.middle.CombinationSum;
 import backtracking.middle.Combinations;
 import backtracking.middle.GenerateParentheses;
 
 public class Main {
     public static void main(String[] args) {
-        Combinations combinations = new Combinations();
-        System.out.println(combinations.combine(4,2));
+        CombinationSum combinationSum = new CombinationSum();
+        System.out.println(combinationSum.combinationSum(new int[]{2,3,6,7},7));
     }
 }
---
+++ src/backtracking/middle/CombinationSum.java
@@ -0,0 +1,32 @@
+package backtracking.middle;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        dfs(combinations,candidates,candidates.length,target,new ArrayList<>(),0,0);
+        return combinations;
+    }
+
+    public void dfs(List<List<Integer>> combinations, int[] candidates, int length, int target, List<Integer> path, int sum, int start){
+        //if the sum equal to the target number, add the path into combinations list
+        if(sum==target){
+            combinations.add(new ArrayList<>(path));
+            return;
+        }
+        //if it is at the last layer, stop recursion
+        if(sum > target){
+            return;
+        }
+        for(int i=start; i<length;i++){
+            //add the number to a temporary path
+            path.add(candidates[i]);
+            dfs(combinations,candidates,length,target,path,sum + candidates[i],i);
+            path.remove(path.size()-1);
+        }
+    }
+}
