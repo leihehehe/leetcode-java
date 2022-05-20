e155f8b70d4f6e89df1a6b2922f5afbd3cff5983
LeiH <hslscarlett@gmail.com>
2022-05-20 16:14:11 +1000

    Gray Code

---
+++ src/GrayCode.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * LeetCode 89
+ */
+public class GrayCode {
+    public List<Integer> grayCode(int n) {
+        List<Integer> results = new ArrayList<>();
+        results.add(0);
+        if(n ==0){
+            return results;
+        }
+        int first=1;
+        for(int i=0;i<n;i++){
+            for(int j= results.size()-1;j>=0;j--){
+                results.add(first+ results.get(j));
+            }
+            first=first<<1;
+        }
+        return results;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import backtracking.middle.*;
 
 public class Main {
     public static void main(String[] args) {
-        WordSearchMethod2 wordSearch = new WordSearchMethod2();
-        char[][] board = {{'a'}};
-        String word = "a";
-        System.out.println(wordSearch.exist(board,word));
+        System.out.println(new GrayCode().grayCode(2));
     }
 
 
