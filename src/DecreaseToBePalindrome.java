9d0e16be853071795d2acecffe7e230ef25aeb72
LeiH <hslscarlett@gmail.com>
2022-06-03 16:25:23 +1000

    lintcode 1784

---
+++ src/DecreaseToBePalindrome.java
@@ -0,0 +1,17 @@
+/***
+ * Lintcode 1784
+ */
+public class DecreaseToBePalindrome {
+    /**
+     * @param s: the string s
+     * @return: the number of operations at least
+     */
+    public int numberOfOperations(String s) {
+        // Write your code here
+        int result=0;
+        for(int i =0;i<s.length()/2;i++){
+            result += Math.abs(s.charAt(i)-s.charAt(s.length()-1-i));
+        }
+        return result;
+    }
+}
