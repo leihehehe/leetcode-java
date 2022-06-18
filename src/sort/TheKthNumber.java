66e5a735d7f268fde0e69f497b5a4fb89f91c301
LeiH <hslscarlett@gmail.com>
2022-06-18 13:31:50 +1000

    quick select

--- src/sort/TheKthNumber.java
+++ src/sort/TheKthNumber.java
@@ -1,7 +1,5 @@
 package sort;
 
-import java.util.Arrays;
-
 /***
  * acwing 786
  * O(n)
@@ -9,30 +7,31 @@ import java.util.Arrays;
  */
 public class TheKthNumber {
 
-    public static int quickSelect(int l, int r, int k,int[] nums){
-        if(l>=r) return nums[l];
-        int i=l-1, j=r+1, p = nums[(l+r)>>1];
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,5,4,2,1};
+        int n =nums.length, k = 2;
+        System.out.println(quickSelect(0,nums.length-1,k,nums));
+
+    }
+
+    public static int quickSelect(int left, int right, int k, int[] nums){
+        if(left>=right) return nums[right];
+        int i =left-1,j = right+1,p = nums[j+(i-j)/2];
         while(i<j){
-            do{i++;} while(nums[i]<p);
-            do{j--;} while(nums[j]>p);
+            do i++; while (nums[i]<p);
+            do j--; while (nums[j]>p);
             if(i<j){
                 int temp = nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
             }
         }
-        if(k<=j){
-            return quickSelect(l,j,k,nums);
-            //do not need to change k, since i, j, l, r all represent index.
+        //the kth number = index k-1
+        if(k-1<=j){
+            return quickSelect(left,j,k,nums);
+        }else{
+            return quickSelect(j+1,right,k,nums);
         }
-        return quickSelect(j+1,r,k,nums);
-    }
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{3,5,4,2,1};
-        int n =nums.length, k = 2;
-        // the Kth number means the index is k-1
-        System.out.println(quickSelect(0,n-1,k-1,nums));
 
     }
 }
