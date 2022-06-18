5a7348ce6b5b82fe5912205cc06a3f13a92e0601
LeiH <hslscarlett@gmail.com>
2022-06-18 20:02:46 +1000

    rename

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -7,12 +7,12 @@ public class MergeSort {
     /***
      * O(nlogn)
      */
-    public static void merge_sort(int l, int r, int[] nums){
+    public static void mergeSort(int l, int r, int[] nums){
         if(l>=r) return;
         int mid = l+(r-l)/2;
         //sort
-        merge_sort(l,mid,nums);
-        merge_sort(mid+1,r,nums);
+        mergeSort(l,mid,nums);
+        mergeSort(mid+1,r,nums);
         //merge
         int[] temp = new int[r-l+1];
         int k=0, i=l, j=mid+1;
@@ -30,7 +30,7 @@ public class MergeSort {
 
     public static void main(String[] args) {
         int[] a = new int[]{3,5,2,1,4,5,3};
-        merge_sort(0,a.length-1,a);
+        mergeSort(0,a.length-1,a);
         System.out.println(Arrays.toString(a));
     }
 
