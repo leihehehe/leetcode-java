73e4024878687f8c58b12d97fcf817e87b65afaa
LeiH <hslscarlett@gmail.com>
2022-06-21 18:30:36 +1000

    check if all the integers in a range are covered

--- src/prefixSumAndDifference/CheckIfAllTheIntegersInARangeAreCovered.java
+++ src/prefixSumAndDifference/CheckIfAllTheIntegersInARangeAreCovered.java
@@ -1,5 +1,26 @@
 package prefixSumAndDifference;
 
+/***
+ * Leetcode 1893
+ * test:
+ * ranges = [[1,2],[3,4],[5,6]], left = 2, right = 5
+ */
 public class CheckIfAllTheIntegersInARangeAreCovered {
-
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        //initialize the difference array
+        int[] diff = new int[51];
+        for(int[] range:ranges){
+            diff[range[0]-1]+=1;
+            diff[range[1]]-=1;
+        }
+        //calculate the prefix sum
+        for(int i =1; i< diff.length;i++){
+            diff[i]+=diff[i-1];
+        }
+        for(int i = left-1;i<right;i++){
+            if(diff[i]<=0)
+                return false;
+        }
+        return true;
+    }
 }
--- src/prefixSumAndDifference/CorporateFlightBookings.java
+++ src/prefixSumAndDifference/CorporateFlightBookings.java
@@ -1,5 +1,23 @@
 package prefixSumAndDifference;
 
+/***
+ * Leetcode 1109
+ */
 public class CorporateFlightBookings {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] diff = new int[n];
+        //diff starts with 0, but booking starts with 1
+        for(int[] booking : bookings){
+            //diff[l]+=c
+            diff[booking[0]-1]+=booking[2];
+            //diff[r+1]-=c
+            if(booking[1]<n) diff[booking[1]]-=booking[2];
 
+        }
+        for(int i =1; i< diff.length;i++){
+            //diff[i-1] has already been re-set to the prefix sum, so we can just add it up.
+            diff[i]+=diff[i-1];
+        }
+        return diff;
+    }
 }
