80bfe1a92e0721a55dd43df477785e8aad6c1cdf
LeiH <hslscarlett@gmail.com>
2022-06-22 15:25:07 +1000

    range addition

---
+++ src/prefixSumAndDifference/RangeAddition.java
@@ -0,0 +1,17 @@
+package prefixSumAndDifference;
+
+public class RangeAddition {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int[] diff = new int[length];
+        for(int[] update: updates){
+            diff[update[0]]+=update[2];
+            if(update[1]+1<length)
+                diff[update[1]+1]-= update[2];
+        }
+
+        for(int i=1; i<length;i++){
+            diff[i]+=diff[i-1];
+        }
+        return diff;
+    }
+}
