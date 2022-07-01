bb3bf725a90ebe96633e9d72fecd40b15f28e3bc
LeiH <hslscarlett@gmail.com>
2022-07-01 16:28:44 +1000

    update sqrare root of x

--- src/binarySearch/SqrtX.java
+++ src/binarySearch/SqrtX.java
@@ -4,6 +4,16 @@ package binarySearch;
  * Leetcode 69
  */
 public class SqrtX {
+    public int mySqrtMethod1(int x) {
+        int l =1, r = x;
+        while(l<r){
+            int mid =l+(r-l+1)/2;
+            if((long)mid*mid > x) r = mid-1;
+            else l = mid;
+        }
+        return r;
+    }
+
     public int mySqrt(int x) {
         int l =0, r= x,aws=-1;
         while(l<=r){
