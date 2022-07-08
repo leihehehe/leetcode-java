1b5e633e3a7987332f6000acaeb92dce42366f9d
LeiH <hslscarlett@gmail.com>
2022-07-08 16:13:47 +1000

    update kth largest elmeent in an array

--- src/sort/KthLargestElementInAnArray.java
+++ src/sort/KthLargestElementInAnArray.java
@@ -22,6 +22,28 @@ public class KthLargestElementInAnArray {
         if (nums.length-k <= j) return quickSort(nums, l, j, k);
         else return quickSort(nums, j + 1, r, k);
     }
+
+    public int findKthLargestMethod2(int[] nums, int k) {
+        int l = 0, r= nums.length-1;
+        //use while to avoid consumption caused by stack
+        while(true){
+            if(l>=r) return nums[r];
+            //[l,i]<=pivot
+            //[j,r]>=pivot
+            int i = l-1, j=r+1, pivot = nums[i+(j-i)/2];
+            while(i<j){
+                //decreasing
+                do i++; while(nums[i]<pivot);
+                do j--; while(nums[j]>pivot);
+                if(i<j)
+                    swap(nums,i,j);
+            }
+            // if the index of the kth largest number(nums.length - k) is smaller and equals to j, go to the left interval, otherwise go to the right one.
+            if (nums.length-k <= j) r=j;
+            else l=j+1;
+        }
+
+    }
     public void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i]=nums[j];
