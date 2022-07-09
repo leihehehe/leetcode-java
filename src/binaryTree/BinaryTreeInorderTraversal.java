7597da3f9cb5a53fe4d2a22c7af59df9d3180021
LeiH <hslscarlett@gmail.com>
2022-07-09 22:46:54 +1000

    inorder traversal

--- src/binaryTree/BinaryTreeInorderTraversal.java
+++ src/binaryTree/BinaryTreeInorderTraversal.java
@@ -13,21 +13,23 @@ import java.util.Stack;
  */
 public class BinaryTreeInorderTraversal {
     public static List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> results=new ArrayList<>();
-        Stack<TreeNode> nodeStack = new Stack<>();
-        TreeNode curNode=root;
-        while (curNode!=null || nodeStack.size()!=0){
-            //check left
-            while (curNode!=null){
-                nodeStack.push(curNode);
-                curNode=curNode.left;
+        List<Integer> res = new ArrayList<>();
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while(cur!=null || !stack.empty()){
+            //left
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
             }
-            //check right
-            curNode=nodeStack.pop();
-            results.add(curNode.val);
-            curNode=curNode.right;
+            //root and right
+            if(!stack.empty()){
+                cur = stack.pop();
+                res.add(cur.val);
+                cur = cur.right;
+            }
         }
-        return results;
+        return res;
     }
     public static void main(String[] args) {
         TreeNode treeNode = new TreeNode(1);
@@ -35,5 +37,19 @@ public class BinaryTreeInorderTraversal {
         treeNode.right=new TreeNode(2);
         treeNode.right.left=new TreeNode(3);
         System.out.println(Arrays.toString(inorderTraversal(treeNode).toArray()));
+    }
+
+    public List<Integer> inorderTraversalRecursive(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root,res);
+        return res;
+    }
+    public void inorder(TreeNode node, List<Integer> res){
+        if(node == null) return;
+        if(node.left!=null)
+            inorder(node.left,res);
+        res.add(node.val);
+        if(node.right!=null)
+            inorder(node.right,res);
     }
 }
