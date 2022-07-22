cbbacd5075fd2eda8e4fefff02b0e9819a4d0880
LeiH <hslscarlett@gmail.com>
2022-07-22 11:02:11 +1000

    simplify path

---
+++ src/stackAndDeque/SimplifyPath.java
@@ -0,0 +1,27 @@
+package stackAndDeque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        String[] paths = path.split("/");
+        Deque<String> deque=new ArrayDeque<String>();
+        for(String p:paths){
+            if(p.equals("") || p.equals(".")) continue;
+            if(p.equals("..")){
+                if(!deque.isEmpty())
+                    deque.removeLast();
+            }
+            else deque.addLast(p);
+        }
+
+        StringBuilder output = new StringBuilder();
+        if(deque.isEmpty()) output.append("/");
+        while(!deque.isEmpty()){
+            String newPath = deque.poll();
+            output.append("/"+newPath);
+        }
+        return output.toString();
+    }
+}
