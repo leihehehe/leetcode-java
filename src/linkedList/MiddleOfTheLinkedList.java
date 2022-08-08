2bad707bcbed2f5628c8a19d380cc4f6f9b837cf
LeiH <hslscarlett@gmail.com>
2022-08-08 11:02:12 +1000

    middle of the linked list

---
+++ src/linkedList/MiddleOfTheLinkedList.java
@@ -0,0 +1,16 @@
+package linkedList;
+
+/***
+ * Leetcode 876
+ */
+public class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode quick = head;
+        ListNode slow = head;
+        while(quick!=null && quick.next!=null){
+            slow = slow.next;
+            quick=quick.next.next;
+        }
+        return slow;
+    }
+}
