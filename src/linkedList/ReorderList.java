451c0cc0bc9c9ecb7bd45d7dd5e15c655957b9ad
LeiH <hslscarlett@gmail.com>
2022-08-11 18:11:34 +1000

    ReorderList Method 2

--- src/linkedList/ReorderList.java
+++ src/linkedList/ReorderList.java
@@ -11,7 +11,7 @@ public class ReorderList {
      * O(N),O(N)
      * @param head
      */
-    public void reorderList(ListNode head) {
+    public void reorderListLinear(ListNode head) {
         ListNode cur = head;
         ArrayList<ListNode> nodes = new ArrayList<>();
         while(cur!=null){
@@ -28,5 +28,54 @@ public class ReorderList {
         }
         nodes.get(i).next=null;
 
+    }
+
+    /***
+     * Method 2:
+     * Step 1: Find middle of the linked list
+     * Step 2: Reserve middle Linked list
+     * Step 3: Merge two linked lists
+     * @param head
+     */
+    public void reorderList(ListNode head) {
+        if(head==null)
+            return;
+        //get the middle node
+        ListNode midNode = findMidNode(head);
+        ListNode head2 = midNode.next;
+        //you have to break it into two linked list, otherwise head1 will have the complete list..
+        midNode.next = null;
+        head2 =reverseLinkedList(head2);
+        ListNode head1 = head;
+        mergelist(head1,head2);
+
+    }
+    public ListNode findMidNode(ListNode head){
+        ListNode slow = head;
+        ListNode quick = head;
+        while(quick!=null && quick.next!=null && quick.next.next!=null){
+            slow = slow.next;
+            quick = quick.next.next;
+        }
+        return slow;
+    }
+    public ListNode reverseLinkedList(ListNode head){
+        if(head==null || head.next ==null) return head;
+        ListNode newHead =reverseLinkedList(head.next);
+        //1->2->3->4
+        //1<-(2<-3<-4) newHead = 4
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+    public void mergelist(ListNode head1, ListNode head2){
+        while(head1!=null&&head2!=null){
+            ListNode head1NextNode=head1.next;
+            ListNode head2NextNode=head2.next;
+            head1.next = head2;
+            head1 = head1NextNode;
+            head2.next=head1;
+            head2= head2NextNode;
+        }
     }
 }
