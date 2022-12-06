863a4af3e6f57dbe568767b49011d807d4cd21a5
LeiH <hslscarlett@gmail.com>
2022-12-06 23:16:51 +0800

    Find Minimum in Rotated Sorted Array method 2

--- src/binarySearch/FindMinimumInRotatedSortedArray.java
+++ src/binarySearch/FindMinimumInRotatedSortedArray.java
@@ -13,4 +13,16 @@ public class FindMinimumInRotatedSortedArray {
         }
         return nums[l];
     }
+
+    public int findMinMethod2(int[] nums) {
+        int l = 0, r = nums.length-1;
+        while(l<r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>nums[r]) l = mid+1;
+            else if(nums[mid]<nums[r]) r= mid;
+                // if nums[mid]==nums[r], mid must be the only number in the current array, just return it.(because there's no duplicates, see Leetcode 154)
+            else return nums[mid];
+        }
+        return nums[l];
+    }
 }
