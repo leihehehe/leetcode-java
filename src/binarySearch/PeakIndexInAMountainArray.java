023be4ac23270ef1d3dc71d2f2854aee80cbb050
LeiH <hslscarlett@gmail.com>
2022-12-08 16:41:05 +0800

    Peak Index in a Mountain Array

---
+++ src/binarySearch/PeakIndexInAMountainArray.java
@@ -0,0 +1,16 @@
+package binarySearch;
+
+/**
+ * Leetcode 852
+ */
+public class PeakIndexInAMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0, r = arr.length-1;
+        while(l<r){
+            int mid = l +(r-l)/2;
+            if(arr[mid]>arr[mid+1]) r=mid;
+            else l = mid+1;
+        }
+        return r;
+    }
+}
