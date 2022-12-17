75a89171e84579df8858145d6a60feb47ebb5adf
LeiH <hslscarlett@gmail.com>
2022-12-17 21:05:20 +0800

    Koko Eating Bananas

---
+++ src/binarySearch/KokoEatingBananas.java
@@ -0,0 +1,31 @@
+package binarySearch;
+
+/**
+ * Leetcode 875
+ */
+public class KokoEatingBananas {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1, r = 0;
+        for(int n : piles){
+            r = Math.max(r,n);
+        }
+
+        while(l<r){
+            int mid = l+(r-l)/2;
+            int time = getTime(piles,mid);
+            if(time>h){
+                l = mid+1;
+            }else{
+                r = mid;
+            }
+        }
+        return l;
+    }
+    public int getTime(int[] piles, int speed){
+        int time = 0;
+        for(int n : piles){
+            time+= (n+speed-1)/speed;
+        }
+        return time;
+    }
+}
