0709c0a6e5e86c38dd3a8511f676bf0f22bb6dc3
LeiH <hslscarlett@gmail.com>
2022-12-22 15:30:12 +0800

    fix bug

--- src/sort/MergeSortedArray.java
+++ src/sort/MergeSortedArray.java
@@ -8,7 +8,7 @@ public class MergeSortedArray {
         int i = 0, j = 0,k=0;
         int[] temp = new int[m+n];
         while(i<m&&j<n){
-            if(nums1[i]<nums2[j]) temp[k++] = nums1[i++];
+            if(nums1[i]<=nums2[j]) temp[k++] = nums1[i++];
             else temp[k++] = nums2[j++];
         }
         while(i<m) temp[k++] = nums1[i++];
