fca19b82f99faa6a2835daefc621061c62f302df
LeiH <hslscarlett@gmail.com>
2022-12-25 14:28:16 +0800

    Quick Sort change

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -33,6 +33,35 @@ public class QuickSort {
         quicksort(j+1,r,a);
     }
 
+    /**
+     * Clearer structure
+     * @param nums
+     * @param l
+     * @param r
+     */
+    public void quickSortSolution2(int[] nums, int l, int r){
+        if(l>=r) return;
+        int pivotIndex = partition(nums,l,r);
+        quickSortSolution2(nums,l,pivotIndex);
+        quickSortSolution2(nums,pivotIndex+1,r);
+    }
+
+    public int partition(int[] nums, int l, int r){
+        //loop invariant: [0,i] < pivot, [j,r] > pivot
+        int pivot = nums[l+(r-l)/2], i = l-1, j = r+1;
+        while(i<j){
+            do i++; while(nums[i]<pivot);
+            do j--; while(nums[j]>pivot);
+            if(i<j) swap(nums,i,j);
+        }
+        return j;
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     public static void main(String[] args) {
         int[] a = new int[]{5,8,4,2,1,3};
         quicksort(0,5,a);
