d716898203fd587d7f1dcd855eb30c7124389853
LeiH <hslscarlett@gmail.com>
2023-02-18 22:13:54 +1100

    House Robber

--- src/dp/HouseRobber.java
+++ src/dp/HouseRobber.java
@@ -23,4 +23,26 @@ public class HouseRobber {
         }
         return dp[n-1];
     }
+
+    /**
+     * TIME: O(N)
+     * SPACE: O(N)
+     * Using two-dimensional array to store the status
+     * @param nums
+     * @return
+     */
+    public int robMethod2(int[] nums) {
+        int n = nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        //dp[i] -> max amount
+        for(int i =1;i<n;i++){
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]);
+            dp[i][1]= dp[i-1][0]+nums[i];
+        }
+        return Math.max(dp[n-1][0],dp[n-1][1]);
+    }
 }
