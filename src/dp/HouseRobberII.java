cf90cadf0adabad1f76dd7d830ab6b41a02e735f
LeiH <hslscarlett@gmail.com>
2023-02-22 11:50:49 +1100

    fix

--- src/dp/HouseRobberII.java
+++ src/dp/HouseRobberII.java
@@ -38,7 +38,7 @@ public class HouseRobberII {
     public int robMethod2(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        return Math.max(robHelperMethod2(nums,0,n-2),robHelper(nums,1,n-1));
+        return Math.max(robHelperMethod2(nums,0,n-2),robHelperMethod2(nums,1,n-1));
     }
     public int robHelperMethod2(int[] nums, int start, int end){
         int[]dp = new int[2];
