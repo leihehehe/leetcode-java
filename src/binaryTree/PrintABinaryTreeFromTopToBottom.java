6e488e993f25eb606e39654da6774873c9efbe2b
LeiH <hslscarlett@gmail.com>
2023-02-24 09:03:07 +1100

    Print a binary tree from top to bottom

---
+++ src/binaryTree/PrintABinaryTreeFromTopToBottom.java
@@ -0,0 +1,29 @@
+package binaryTree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+/**
+ * Leetcode offer 32
+ */
+public class PrintABinaryTreeFromTopToBottom {
+    public int[] levelOrder(TreeNode root) {
+        if(root==null) return new int[0];
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        ArrayList<Integer> temp = new ArrayList<>();
+        while(!queue.isEmpty()){
+            TreeNode curNode = queue.poll();
+            temp.add(curNode.val);
+            if(curNode.left!=null) queue.add(curNode.left);
+            if(curNode.right!=null) queue.add(curNode.right);
+        }
+        int size = temp.size();
+        int[] res = new int[size];
+        for(int i = 0;i<size;i++){
+            res[i]=temp.get(i);
+        }
+        return res;
+    }
+}
