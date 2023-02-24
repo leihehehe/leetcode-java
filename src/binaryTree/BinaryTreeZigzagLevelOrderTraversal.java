bc8e1fd45b3190649d125d22c6d10fbdd2062d21
LeiH <hslscarlett@gmail.com>
2023-02-24 11:39:16 +1100

    Binary Tree Zigzag Level Order Traversal

---
+++ src/binaryTree/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,41 @@
+package binaryTree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Leetcode 103
+ * the same as offer 32
+ */
+public class BinaryTreeZigzagLevelOrderTraversal {
+    /**
+     * Time: O(N)
+     * Space: O(N)
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int layer = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> curLevel = new ArrayList<>();
+            for(int i =0;i<size;i++){
+                TreeNode cur = queue.poll();
+                if(layer%2==0){
+                    curLevel.add(cur.val);
+                }else curLevel.add(0,cur.val);
+                if(cur.left!=null) queue.offer(cur.left);
+                if(cur.right!=null) queue.offer(cur.right);
+            }
+            res.add(curLevel);
+            layer++;
+        }
+        return res;
+    }
+}
