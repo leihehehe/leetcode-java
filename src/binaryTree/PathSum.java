a5e5df7c285bd50fa900f94e26517fd976ce67c7
LeiH <hslscarlett@gmail.com>
2023-02-27 23:06:51 +1100

    Path Sum

--- src/binaryTree/PathSum.java
+++ src/binaryTree/PathSum.java
@@ -1,6 +1,15 @@
 package binaryTree;
 
+/**
+ * Leetcode 112
+ */
 public class PathSum {
+    /**
+     * Method 1
+     * @param root
+     * @param targetSum
+     * @return
+     */
     public boolean hasPathSum(TreeNode root, int targetSum) {
         return checkPathSum(root,0,targetSum);
     }
@@ -10,5 +19,28 @@ public class PathSum {
         sum+=node.val;
         if(node.left==null && node.right ==null && sum==targetSum) return true;
         return checkPathSum(node.left,sum,targetSum) || checkPathSum(node.right,sum,targetSum);
+    }
+
+    /**
+     * Method2
+     */
+
+    int targetSum=0;
+    boolean res = false;
+    public boolean hasPathSumMethod2(TreeNode root, int targetSum) {
+        if(root==null) return res;
+        this.targetSum=targetSum;
+        dfs(root,0);
+        return res;
+    }
+    public void dfs(TreeNode root,int sum){
+        if(root==null) return;
+        sum+=root.val;
+        if(sum==targetSum && root.left==null && root.right==null){
+            res= true;
+            return;
+        }
+        dfs(root.left,sum);
+        dfs(root.right,sum);
     }
 }
