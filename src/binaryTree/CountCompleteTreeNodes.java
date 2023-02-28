c5b5e371ab564b2ad0b7a894577d8bbe12e2321d
LeiH <hslscarlett@gmail.com>
2023-02-28 12:58:11 +1100

    Count Complete Tree Nodes

--- src/binaryTree/CountCompleteTreeNodes.java
+++ src/binaryTree/CountCompleteTreeNodes.java
@@ -27,4 +27,45 @@ public class CountCompleteTreeNodes {
         if(h0==h1) return (int)Math.pow(2,h0)-1;
         return 1+countNodes(root.left)+countNodes(root.right);
     }
+
+    private int count;
+    /**
+     * Method 2: recursive method
+     * Time complexity: O(logN^2)
+     * Time complexity: O(logN)
+     * @param root
+     * @return
+     */
+    public int countNodesMethod2(TreeNode root) {
+        recursive(root);
+        return count;
+
+    }
+
+    public void recursive(TreeNode curNode) {
+        if (curNode == null) {
+            return;
+        }
+        int h1=0, h2=0;
+        TreeNode node = curNode;
+        //logN
+        while(node!=null){
+            node = node.left;
+            h1++;
+        }
+        node = curNode;
+        //logN
+        while(node!=null){
+            node = node.right;
+            h2++;
+        }
+        if(h1==h2){
+            count+=(int)Math.pow(2,h1)-1;
+        }else{
+            count++;
+            //logN
+            recursive(curNode.left);
+            recursive(curNode.right);
+        }
+    }
 }
