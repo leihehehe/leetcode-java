a6ba88f07abef1d62231415b507061f5dc0b4668
LeiH <hslscarlett@gmail.com>
2023-03-01 16:02:44 +1100

    Construct Binary Tree from Inorder and Postorder Traversal

--- src/binaryTree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/binaryTree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -7,11 +7,43 @@ import java.util.Map;
  * Leetcode 106
  */
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
+    /**
+     * Method 1
+     */
+    //inorder = [9,3,15,20,7]
+    //           l ro   r
+    //          pS ls      pE
+    //postorder = [9,15,7,20,3]
+    //             l r       ro
+    //            pS         pE
+    Map<Integer,Integer> indexes = new HashMap<>();
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        for(int i =0;i<inorder.length;i++){
+            indexes.put(inorder[i],i);
+        }
+        return build(inorder,0,inorder.length-1,postorder,0,postorder.length-1);
+    }
+
+    public TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd){
+        if(inStart>inEnd) return null;
+        int val = postorder[postEnd];
+        TreeNode root = new TreeNode(val);
+        //index in inorder
+        int index = indexes.get(val);
+        int leftSize = index-inStart;
+        root.left = build(inorder,inStart,index-1,postorder,postStart,postStart+leftSize-1);
+        root.right = build(inorder,index+1,inEnd,postorder,postStart+leftSize,postEnd-1);
+        return root;
+    }
+
+    /**
+     * Method 2
+     */
     private int[] inorder;
     private int[] postorder;
     private int postIndex;
     Map<Integer,Integer> storeMap = new HashMap<Integer,Integer>();
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
+    public TreeNode buildTreeMethod2(int[] inorder, int[] postorder) {
         this.inorder=inorder;
         this.postorder=postorder;
         //put inorder<node,index> into a map
