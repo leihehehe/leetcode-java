3b76248b679d273e31a47af6690501422073fec1
LeiH <hslscarlett@gmail.com>
2023-03-02 21:26:37 +1100

    Reverse String

--- src/dataStructure/array/ContainsDuplicate.java
+++
@@ -1,4 +0,0 @@
-package dataStructure.array;
-
-public class ContainsDuplicate {
-}
---
+++ src/doublePointer/ReverseString.java
@@ -0,0 +1,20 @@
+package doublePointer;
+
+/**
+ * Leetcode 344
+ */
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length-1;
+        while(i<j){
+            swap(s,i,j);
+            i++;
+            j--;
+        }
+    }
+    public void swap(char[] s, int i, int j ){
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
