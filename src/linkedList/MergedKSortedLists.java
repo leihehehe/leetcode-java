b54ec68a3943f5be2d33de8803f0bf5b4a28c28b
LeiH <hslscarlett@gmail.com>
2023-03-02 12:35:59 +1100

    Merged K sorted Lists fixed

--- src/linkedList/MergedKSortedLists.java
+++ src/linkedList/MergedKSortedLists.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import java.util.PriorityQueue;
+
 /***
  * Leetcode 23
  */
@@ -12,34 +14,30 @@ public class MergedKSortedLists {
      * @return
      */
     public ListNode mergeKLists(ListNode[] lists) {
-        return merge(lists, 0, lists.length - 1);
+        return merge(lists,0,lists.length-1);
     }
-
-    public ListNode merge(ListNode[] lists, int l, int r) {
-        if (l == r) {
-            return lists[l];
-        }
-        if (l > r) {
-            return null;
-        }
-        int mid = l-(r+l)/2;
-        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    public ListNode merge(ListNode[] lists, int l, int r){
+        if(l==r) return lists[l];
+        if(l>r) return null;
+        int mid = l+(r-l)/2;
+        return mergeTwoLists(merge(lists,l,mid),merge(lists,mid+1,r));
     }
     public ListNode mergeTwoLists(ListNode list1, ListNode list2){
-        ListNode dummyNode = new ListNode(-1);
-        ListNode res =dummyNode;
-        while(list1!=null && list2!=null){
-            if(list1.val<=list2.val){
-                dummyNode.next=list1;
-                list1=list1.next;
+        ListNode dummyNode = new ListNode(-1), p = dummyNode;
+        ListNode p1 = list1, p2 = list2;
+        while(p1!=null && p2!=null){
+            if(p1.val<=p2.val){
+                p.next=p1;
+                p1=p1.next;
             }else{
-                dummyNode.next=list2;
-                list2=list2.next;
+                p.next = p2;
+                p2=p2.next;
             }
-            dummyNode=dummyNode.next;
+            p = p.next;
         }
-        dummyNode.next = list1==null?list2:list1;
-        return res.next;
+        if(p1!=null) p.next = p1;
+        if(p2!=null) p.next = p2;
+        return dummyNode.next;
     }
     /***
      * Method 2: a straight-forward way: iterate each linkedlist and merge
@@ -66,5 +64,28 @@ public class MergedKSortedLists {
         }
         dummyNode.next = list1==null?list2:list1;
         return res.next;
+    }
+
+    /**
+     * Method 3: priority queue
+     * Time complexity: O(n logk)
+     * Space complexity: O(k)->priority queue
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKListsMethod3(ListNode[] lists) {
+        if(lists.length==0) return null;
+        ListNode dummy = new ListNode(-1), p = dummy;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length,(a, b)->a.val-b.val);
+        for(ListNode head : lists){
+            if(head!=null) pq.offer(head);
+        }
+        while(!pq.isEmpty()){
+            ListNode node = pq.poll();
+            p.next = node;
+            if(node.next!=null) pq.offer(node.next);
+            p=p.next;
+        }
+        return dummy.next;
     }
 }
