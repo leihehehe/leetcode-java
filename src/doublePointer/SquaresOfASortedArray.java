b8fbf95c6a345e2e11f411ca6fa379b43c292bbd
LeiH <hslscarlett@gmail.com>
2023-03-23 15:48:47 +1100

    977. Squares of a Sorted Array

---
+++ src/doublePointer/SquaresOfASortedArray.java
@@ -0,0 +1,24 @@
+package doublePointer;
+
+/**
+ * Leetcode 977
+ */
+public class SquaresOfASortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int[] res = new int[nums.length];
+        int p = res.length-1;
+        int l = 0, r = nums.length-1;
+        while(l<=r){
+            if(nums[l]*nums[l] < nums[r]*nums[r]){
+                res[p] = nums[r]*nums[r];
+                p--;
+                r--;
+            }else{
+                res[p] = nums[l]*nums[l];
+                p--;
+                l++;
+            }
+        }
+        return res;
+    }
+}
