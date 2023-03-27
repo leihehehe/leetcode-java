db38d40ac1fe0a92d55d348b6d4ff5204132a552
LeiH <hslscarlett@gmail.com>
2023-03-27 16:41:13 +1100

    785. Is Graph Bipartite

--- src/graph/IsGraphBipartite.java
+++ src/graph/IsGraphBipartite.java
@@ -1,5 +1,8 @@
 package graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Leetcode 785
  */
@@ -18,6 +21,11 @@ public class IsGraphBipartite {
         return bipartite;
     }
 
+    /**
+     * Method 1: dfs
+     * @param graph
+     * @param v
+     */
     public void traverse(int[][] graph, int v){
         //if not bipartite
         if(!bipartite) return;
@@ -35,4 +43,31 @@ public class IsGraphBipartite {
 
         }
     }
+
+    /**
+     * Method 2: BFS
+     * @param graph
+     * @param start
+     */
+    public void traverseBFS(int[][] graph, int start){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+        while(!queue.isEmpty() ){
+            int v = queue.poll();
+            for(int neighbour:graph[v]){
+                if(!visited[neighbour]){
+                    color[neighbour]=!color[v];
+                    visited[neighbour]=true;
+                    queue.offer(neighbour);
+                }else{
+                    if(color[neighbour]==color[v]){
+                        bipartite = false;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
 }
