af9226797bc577d7d7ed4c1cb4e407b342030274
LeiH <hslscarlett@gmail.com>
2023-04-16 15:03:56 +1000

    [1905]Count Sub Islands

---
+++ src/dfs/[1905]Count Sub Islands.java
@@ -0,0 +1,34 @@
+package dfs;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class CountSubIslands {
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length, n = grid1[0].length;
+        for(int i = 0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(grid2[i][j]==1 && grid1[i][j]==0) dfs(grid2,i,j);
+            }
+        }
+        int res = 0;
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(grid2[i][j]==1){
+                    res++;
+                    dfs(grid2,i,j);
+                }
+            }
+        }
+        return res;
+    }
+    public void dfs(int[][] grid, int i, int j){
+        int m = grid.length, n = grid[0].length;
+        if(i<0 || j<0 || i>=m || j>= n) return;
+        if(grid[i][j]==0) return;
+        grid[i][j] = 0;
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
