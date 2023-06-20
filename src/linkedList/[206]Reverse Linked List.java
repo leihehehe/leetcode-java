c00c4adffd44bcfb87b532fb8e9a24d2c272c518
LeiH <hslscarlett@gmail.com>
2023-06-20 16:07:56 +1000

    [206]Reverse Linked List

--- src/linkedList/[206]Reverse Linked List.java
+++ src/linkedList/[206]Reverse Linked List.java
@@ -1,9 +1,15 @@
 package linkedList;
-
-/***
- * LeetCode 206
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
-public class ReverseLinkedList {
+class ReverseLinkedList {
     public ListNode reverseListRecursion(ListNode head) {
         if(head==null || head.next == null) return head;
         ListNode newHead = reverseListRecursion(head.next);
