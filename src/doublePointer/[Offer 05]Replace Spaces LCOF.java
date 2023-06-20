04f2535b3bd9ba2721cc33e0aabf3c310d8dccd5
LeiH <hslscarlett@gmail.com>
2023-06-21 09:08:31 +1000

    offer 05

---
+++ src/doublePointer/[Offer 05]Replace Spaces LCOF.java
@@ -0,0 +1,18 @@
+package doublePointer;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class ReplaceSpace {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c==' ')
+                sb.append("%20");
+            else
+                sb.append(c);
+        }
+        return sb.toString();
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
