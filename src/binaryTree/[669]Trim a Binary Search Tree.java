21b1c8157b710f89e7b03e5dacd01e83658a5095
LeiH <hslscarlett@gmail.com>
2023-06-24 19:48:40 +1000

    [669]Trim a Binary Search Tree

---
+++ src/binaryTree/[669]Trim a Binary Search Tree.java
@@ -0,0 +1,35 @@
+package binaryTree;
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TrimABinarySearchTree {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root==null)
+            return null;
+        else if(root.val<low){
+            return trimBST(root.right,low,high);
+        }
+        else if (root.val>high){
+            return trimBST(root.left,low,high);
+        }
+        else{
+            root.left = trimBST(root.left,low,high);
+            root.right = trimBST(root.right,low,high);
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
