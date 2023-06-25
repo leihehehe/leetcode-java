534e2b4f8f341a8730c7981b3cb251d65b51c933
LeiH <hslscarlett@gmail.com>
2023-06-25 18:17:04 +1000

    Unique Paths II

--- src/dp/[63]Unique Paths II.java
+++ src/dp/[63]Unique Paths II.java
@@ -3,7 +3,38 @@ package dp;
 /**
  * Leetcode 63
  */
-public class UniquePathsII {
+class UniquePathsIISolutionI{
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i =0;i<m;i++){
+            if(obstacleGrid[i][0]==1)
+                break;
+            dp[i][0] = 1;
+        }
+        for(int i = 0;i<n;i++){
+            if(obstacleGrid[0][i]==1)
+                break;
+            dp[0][i] = 1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                if(obstacleGrid[i][j]==1)
+                    continue;
+                if(obstacleGrid[i-1][j]==0){
+                    dp[i][j]+=dp[i-1][j];
+                }
+                if(obstacleGrid[i][j-1]==0){
+                    dp[i][j] += dp[i][j-1];
+                }
+
+            }
+        }
+        return dp[m-1][n-1];
+
+    }
+}
+class UniquePathsIISolutionII {
     /**
      * Method 1: double dimensional array
      * TIME: O(m*n)
