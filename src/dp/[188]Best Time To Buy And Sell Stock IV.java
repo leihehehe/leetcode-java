639d95b81af9b41cdbe2abceb76b30f027fad1b3
LeiH <hslscarlett@gmail.com>
2023-06-30 14:20:41 +1000

    [188]Best Time To Buy And Sell Stock IV

--- src/dp/[188]Best Time To Buy And Sell Stock IV.java
+++ src/dp/[188]Best Time To Buy And Sell Stock IV.java
@@ -1,6 +1,25 @@
 package dp;
 
-public class BestTimeToBuyAndSellStockIV {
+class BestTimeToBuyAndSellStockIVSolution1{
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n][k+1][2];
+        for(int i = 0;i<n;i++){
+            for(int j=k;j>=1;j--){
+                if(i==0){
+                    dp[0][j][0] = 0;
+                    dp[0][j][1] = -prices[0];
+                }else{
+                    dp[i][j][0] = Math.max(dp[i-1][j][0],dp[i-1][j][1]+prices[i]);
+                    dp[i][j][1] = Math.max(dp[i-1][j][1],dp[i-1][j-1][0]-prices[i]);
+                }
+
+            }
+        }
+        return dp[n-1][k][0];
+    }
+}
+class BestTimeToBuyAndSellStockIVSolution2 {
     /**
      * Method 1: dynamic programming
      * TIME: O(NK)
