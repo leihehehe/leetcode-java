8cd8f1e13d5be5f69d8d1c4c705a8a777ee5cf8c
LeiH <hslscarlett@gmail.com>
2023-07-01 10:40:04 +1000

    [139] word Break

--- src/dp/[139]Word Break.java
+++ src/dp/[139]Word Break.java
@@ -4,25 +4,30 @@ import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class WordBreak {
+    /**
+     * 序列dp，非完全背包。完全背包都是无序的
+     * @param s
+     * @param wordDict
+     * @return
+     */
     public boolean wordBreak(String s, List<String> wordDict) {
-        //考虑s是否能由wordDict得到
-        boolean[] dp = new boolean[s.length()+1];
+        int len = s.length();
+        boolean[] dp = new boolean[len+1];
         dp[0] = true;
-        for(int i =1;i<=s.length();i++){
-            //判断单词
-            for(int j = 0;j<wordDict.size();j++){
-                String word = wordDict.get(j);
-                int len = word.length();
-                //检查i-len,i这一段是不是可以选，检查之前的是不是true
-                if(i>=len && s.substring(i-len,i).equals(word)){
-                    dp[i]=dp[i-len];
+        for(int i =1;i<=len;i++){
+            for(String word : wordDict){
+                //选或不选的问题
+                //选
+                int wordLen = word.length();
+                if(i-wordLen>=0 && s.substring(i-wordLen,i).equals(word)){
+                    dp[i] = dp[i] || dp[i-wordLen];
+                    //根据dp[i]的定义=》 前i个字符是否能被拆分。一旦我们确定 dp[i] 是 true，我们就不需要再检查其他单词了
                     if(dp[i]) break;
-
                 }
 
             }
         }
-        return dp[s.length()];
+        return dp[len];
     }
 
 
