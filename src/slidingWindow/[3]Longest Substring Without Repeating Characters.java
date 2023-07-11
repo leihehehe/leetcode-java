6fdb2e8cea4b069df9ad67f4b763c81c8409fa41
LeiH <hslscarlett@gmail.com>
2023-07-11 11:28:37 +1000

    [3]Longest Substring Without Repeating Characters

--- src/slidingWindow/[3]Longest Substring Without Repeating Characters.java
+++ src/slidingWindow/[3]Longest Substring Without Repeating Characters.java
@@ -21,5 +21,34 @@ class LongestSubstringWithoutRepeatingCharacters {
         }
         return res;
     }
+
+    /**
+     * Dp solution
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstringDP(String s) {
+        int n = s.length();
+        if(n==0) return 0;
+        //dp[i]以s[i]结尾的最长无重复字符
+        int[] dp = new int[n];
+        dp[0] = 1;
+        //最后一次出现的位置
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put(s.charAt(0),0);
+        int maxLen = 1;
+        for(int i = 1;i<n;i++){
+            char ch = s.charAt(i);
+            if(!map.containsKey(ch) || i-map.get(ch)>dp[i-1]){
+                dp[i] = dp[i-1]+1;
+            }else{
+                //计算最后一次出现的位置到现在的距离，就是最长无重复子串长度
+                dp[i] = i-map.get(ch);
+            }
+            map.put(ch,i);
+            maxLen = Math.max(dp[i],maxLen);
+        }
+        return maxLen;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
