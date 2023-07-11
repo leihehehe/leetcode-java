719eaef0ceccade35aff5a0a7da080a603c23fb9
LeiH <hslscarlett@gmail.com>
2023-07-11 16:32:11 +1000

    [395]Longest Substring with At Least K Repeating Characters

--- src/slidingWindow/[395]Longest Substring with At Least K Repeating Characters.java
+++ src/slidingWindow/[395]Longest Substring with At Least K Repeating Characters.java
@@ -1,37 +1,48 @@
 package slidingWindow;
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class LongestSubstringWithAtLeastKRepeatingCharacters {
-    public int longestSubstring(String s, int k) {
-        int res=0;
-        for(int i = 1;i<=26;i++){
-            res = Math.max(res,longestSubstringWithNCharacters(s,k,i));
-        }
-        return res;
-    }
+import java.util.HashMap;
 
-    public int longestSubstringWithNCharacters(String s, int k, int n){
-        int[] window = new int[26];
-        int l = 0, r= 0;
-        int charactersCount = 0;
-        int validCharacters = 0;
-        int res = 0;
-        while(r<s.length()){
-            int toAdd = s.charAt(r)-'a';
-            r++;
-            if(window[toAdd]==0) charactersCount++;
-            window[toAdd]=window[toAdd]+1;
-            if(window[toAdd]==k) validCharacters++;
-            while(charactersCount>n){
-                int toRemove = s.charAt(l)-'a';
-                l++;
-                if(window[toRemove]==k) validCharacters--;
-                window[toRemove]=window[toRemove]-1;
-                if(window[toRemove]==0) charactersCount--;
-            }
-            if(validCharacters==n) res=Math.max(res,r-l);
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int maxLen = 0;
+        for(int i = 1;i<=26;i++){
+            maxLen = Math.max(maxLen,longestSubString(s,k,i));
         }
-        return res;
+        return maxLen;
+    }
+    //这里的count是规定了有多少种类型的字符
+    public int longestSubString(String s, int k, int count){
+        int n = s.length();
+        HashMap<Character,Integer> map = new HashMap<>();
+        int l = 0, r = 0;
+        int meet = 0;
+        int maxLen = 0;
+        while(r<n){
+            char toAdd = s.charAt(r);
+            r++;
+            map.put(toAdd,map.getOrDefault(toAdd,0)+1);
+            //一个字符已经满足条件
+            if(map.get(toAdd)==k){
+                meet++;
+            }
+            //如果说窗口内的字符种类>指定的count种类
+            while(map.size()>count){
+                char toRemove = s.charAt(l);
+                l++;
+                map.put(toRemove,map.get(toRemove)-1);
+                if(map.get(toRemove)==k-1){
+                    meet--;
+                }
+                if(map.get(toRemove)==0){
+                    map.remove(toRemove);
+                }
+            }
+            if(meet==count){
+                maxLen = Math.max(r-l,maxLen);
+            }
+        }
+        return maxLen;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
