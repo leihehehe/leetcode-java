00aaf2a7e32ea2596d9221b694a99b342b506998
LeiH <hslscarlett@gmail.com>
2023-07-12 16:47:49 +1000

    [1004]Max Consecutive Ones III

--- src/slidingWindow/[1004]Max Consecutive Ones III.java
+++ src/slidingWindow/[1004]Max Consecutive Ones III.java
@@ -2,7 +2,31 @@ package slidingWindow;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class MaxConsecutiveOnesIII {
-    public int longestOnes(int[] nums, int k) {
+    public int longestOnesMethod1(int[] nums, int k) {
+        int l = 0, r = 0;
+        int countZero = 0;
+        int maxLen = 0;
+        while(r<nums.length){
+            int toAdd = nums[r];
+            r++;
+            if(toAdd==0){
+                countZero++;
+            }
+            if(countZero<=k){
+                maxLen = Math.max(r-l,maxLen);
+            }
+            if(countZero>k){
+                int toRemove = nums[l];
+                l++;
+                if(toRemove==0){
+                    countZero--;
+                }
+            }
+
+        }
+        return maxLen;
+    }
+    public int longestOnesMethod2(int[] nums, int k) {
         int l =0, r = 0;
         int count = 0;
         int res = 0;
