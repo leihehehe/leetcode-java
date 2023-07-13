3a5af8b03ca602d1c48b1631cd5420f28fd311f3
LeiH <hslscarlett@gmail.com>
2023-07-13 17:10:59 +1000

    [852]Peak Index In A Mountain Array

--- src/binarySearch/[852]Peak Index In A Mountain Array.java
+++ src/binarySearch/[852]Peak Index In A Mountain Array.java
@@ -3,7 +3,7 @@ package binarySearch;
 /**
  * Leetcode 852
  */
-public class PeakIndexInAMountainArray {
+class PeakIndexInAMountainArray {
     public int peakIndexInMountainArray(int[] arr) {
         int l = 0, r = arr.length-1;
         while(l<r){
