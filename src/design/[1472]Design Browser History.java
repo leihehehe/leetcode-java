4f4079a8d45bf12816f5679592a604f2d3561f47
LeiH <hslscarlett@gmail.com>
2023-07-14 18:22:56 +1000

    [1472]Design Browser History

--- src/design/[1472]Design Browser History.java
+++ src/design/[1472]Design Browser History.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class BrowserHistory {
+class BrowserHistoryMethod1 {
     private String url;
     private Deque<String> backHistory;
     private Deque<String> forwardHistroy;
@@ -14,7 +14,7 @@ class BrowserHistory {
      * the tail of the queue will be the most recently visited url
      * 1-2-3-4
      */
-    public BrowserHistory(String homepage) {
+    public BrowserHistoryMethod1(String homepage) {
         url = homepage;
         backHistory = new ArrayDeque<>();
         forwardHistroy = new ArrayDeque<>();
@@ -54,7 +54,51 @@ class BrowserHistory {
         return url;
     }
 }
+class BrowserHistoryMethod2 {
+    private String url;
+    private String[] history;
+    private int cur;
+    private int tail;
+    /**
+     * the most suitable ds for this linkedlist.
+     * the head of the queue will be the least recently visted url
+     * the tail of the queue will be the most recently visited url
+     * [1,2,3,4]
+     */
+    public BrowserHistoryMethod2(String homepage) {
+        url = homepage;
+        history = new String[5000];
+        cur = 0;
+        tail = 0;
+        //[head,tail]
+        history[cur] = homepage;
+    }
 
+    public void visit(String url) {
+        history[++cur] = url;
+        tail = cur;
+    }
+
+    public String back(int steps) {
+        for(int i =1;i<=steps;i++){
+            if(cur-1<0){
+                break;
+            }
+            cur--;
+        }
+        return history[cur];
+    }
+
+    public String forward(int steps) {
+        for(int i =1;i<=steps;i++){
+            if(cur+1>tail){
+                break;
+            }
+            cur++;
+        }
+        return history[cur];
+    }
+}
 /**
  * Your BrowserHistory object will be instantiated and called as such:
  * BrowserHistory obj = new BrowserHistory(homepage);
