8e445e6dda2a0eb59809299283705c65a7c3730d
LeiH <hslscarlett@gmail.com>
2023-07-23 15:46:25 +1000

    [227]Basic Calculator II

---
+++ src/design/[227]Basic Calculator II.java
@@ -0,0 +1,38 @@
+package design;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class BasicCalculatorII {
+    public int calculate(String s) {
+        //3+2*2
+        s = s.replace(" ", "");
+        Deque<Integer> stack = new ArrayDeque<>();
+        char sign = '+';
+        int num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            if(!Character.isDigit(c) || i==s.length()-1){
+                switch (sign) {
+                    case '+' -> stack.push(num);
+                    case '-' -> stack.push(-num);
+                    case '*' -> stack.push(stack.pop() * num);
+                    case '/' -> stack.push(Math.floorDiv(stack.pop(), num));
+                }
+                sign = c;
+                num = 0;
+            }
+        }
+        int res = 0;
+        while (!stack.isEmpty()) {
+            res += stack.pop();
+        }
+        return res;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
