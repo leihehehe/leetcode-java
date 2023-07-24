be9ae7cd252ebbbe21abd6e0501aa3d0006c8ec7
LeiH <hslscarlett@gmail.com>
2023-07-24 19:32:25 +1000

    [353]Design Snake Game

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,13 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedHashMap;
+import java.util.Objects;
 
 public class Test {
 
     public static void main(String[] args) {
         System.out.println(-5/-2);
     }
+
+
 }
--- src/design/[353]Design Snake Game.java
+++ src/design/[353]Design Snake Game.java
@@ -1,11 +1,9 @@
 package design;
 
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class SnakeGame {
+class SnakeGame1 {
     private Deque<Integer> body = new LinkedList<>();
     private HashSet<Integer> set = new HashSet<>();
     private int width;
@@ -13,7 +11,7 @@ class SnakeGame {
     private boolean isAlive;
     //这个food一次只出现一个
     private LinkedList<Integer> food;
-    public SnakeGame(int width, int height, int[][] food) {
+    public SnakeGame1(int width, int height, int[][] food) {
         this.width = width;
         this.height = height;
         body.addFirst(encode(0,0));
@@ -85,3 +83,93 @@ class SnakeGame {
  * int param_1 = obj.move(direction);
  */
 //leetcode submit region end(Prohibit modification and deletion)
+class SnakeGame2 {
+    private LinkedList<Position> body;
+    private HashSet<Position> set;
+    private int width;
+    private int height;
+    private boolean alive;
+    private Deque<Position> foodPos;
+    private int score;
+    public SnakeGame2(int width, int height, int[][] food) {
+        body = new LinkedList<>();
+        body.addLast(new Position(0,0));
+        set = new HashSet<>();
+        set.add(new Position(0,0));
+        this.width = width;
+        this.height = height;
+        alive = true;
+        foodPos = new ArrayDeque<>();
+        for(int[] piece: food){
+            foodPos.addLast(new Position(piece[1],piece[0]));
+        }
+        score = 0;
+    }
+    //returns score of the game after applying one direction move by the snake
+    public int move(String direction) {
+        if(!alive){
+            return -1;
+        }
+        //check bounds
+        Position head = body.getLast();
+        int x = head.x;
+        int y = head.y;
+        switch(direction){
+            case "U" -> y = y-1;
+            case "R" -> x = x+1;
+            case "D" -> y = y+1;
+            case "L" -> x = x-1;
+        }
+        Position curPos = new Position(x,y);
+        if(x<0 || x>=width || y<0 || y>=height){
+            alive = false;
+            return -1;
+        }
+        Position curFood = foodPos.peekFirst();
+        if(curFood!=null && curFood.equals(curPos)){
+            //不pop尾巴
+            foodPos.pollFirst();
+            score++;
+        }else{
+            Position p = body.removeFirst();
+            set.remove(p);
+        }
+        //碰到body -> 移除蛇尾后检查！
+        if(set.contains(curPos)){
+            alive = false;
+            return -1;
+        }
+        body.addLast(curPos);
+        set.add(curPos);
+        return score;
+    }
+    class Position{
+        private int x;
+        private int y;
+        public Position(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        @Override
+        public boolean equals(Object pos){
+            if(!(pos instanceof Position)){
+                return false;
+            }
+            if(pos == this){
+                return true;
+            }
+            Position p = (Position) pos;
+            return p.x==x && p.y ==y;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(x,y);
+        }
+    }
+}
+
+/**
+ * Your SnakeGame object will be instantiated and called as such:
+ * SnakeGame obj = new SnakeGame(width, height, food);
+ * int param_1 = obj.move(direction);
+ */
