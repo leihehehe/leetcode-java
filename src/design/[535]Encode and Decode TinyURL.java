2d980126b294fd84be1bc4109e00eadf3075e517
LeiH <hslscarlett@gmail.com>
2023-07-26 00:24:26 +1000

    [535]Encode and Decode TinyURL

--- src/design/[535]Encode and Decode TinyURL.java
+++ src/design/[535]Encode and Decode TinyURL.java
@@ -1,5 +1,9 @@
 package design;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -7,13 +11,25 @@ import java.util.Objects;
 class Codec {
     private HashMap<String,String> map = new HashMap<>();
     private static final String PREFIX = "http://tinyurl.com/";
+    private static final String BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
         String[] str = longUrl.split("/");
         String code = str[str.length-1];
-        String encodedCode = String.valueOf(Objects.hash(code));
+        int hash = Objects.hash(code);
+        String encodedCode = base62Encode(hash);
         map.put(encodedCode,longUrl);
         return PREFIX+encodedCode;
+
+    }
+
+    public static String base62Encode(int value){
+        StringBuilder sb = new StringBuilder();
+        while(value!=0){
+            sb.append(BASE62.charAt(value%62));
+            value/=62;
+        }
+        return sb.reverse().toString();
     }
 
     // Decodes a shortened URL to its original URL.
@@ -21,6 +37,13 @@ class Codec {
         String[] str = shortUrl.split("/");
         String code = str[str.length-1];
         return map.get(code);
+    }
+
+    public static void main(String[] args) {
+        Codec codec = new Codec();
+        String encode = codec.encode("http://baidu.com/shulei123213asdsadwadwqd");
+        System.out.println(encode);
+        System.out.println(codec.decode(encode));
     }
 }
 
