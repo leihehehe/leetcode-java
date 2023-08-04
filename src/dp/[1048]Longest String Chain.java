d018c202e71e50d363aaadae12ec8cf2fa91ea83
LeiH <hslscarlett@gmail.com>
2023-08-04 14:44:38 +1000

    [1048]Longest String Chain

--- src/dp/[1048]Longest String Chain.java
+++ src/dp/[1048]Longest String Chain.java
@@ -2,9 +2,10 @@ package dp;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class LongestStringChain {
+class LongestStringChainSolution1 {
     public int longestStrChain(String[] words) {
         Arrays.sort(words,(a, b)->a.length()-b.length());
         int n = words.length;
@@ -22,6 +23,40 @@ class LongestStringChain {
             maxLen = Math.max(maxLen,curLen);
         }
         return maxLen;
+    }
+
+
+}
+
+class LongestStringChainSolution2 {
+    Map<String,Integer> map = new HashMap<>();
+    public int longestStrChain(String[] words) {
+
+        for(int i = 0;i<words.length;i++){
+            map.put(words[i],0);
+        }
+        int res = 0;
+        for(int i = 0;i<words.length;i++){
+            res = Math.max(res,dfs(words[i]));
+        }
+        return res;
+    }
+
+    //当word是chain中的最后一个单词，长度是多少？
+    public int dfs(String word){
+        int res = map.getOrDefault(word,0);
+        if(res>0){
+            return res;
+        }
+        for(int i = 0;i<word.length();i++){
+            String newStr = word.substring(0,i)+word.substring(i+1);
+            if(map.containsKey(newStr)){
+                res= Math.max(res,dfs(newStr));
+            }
+        }
+        res+=1;
+        map.put(word,res);
+        return res;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
