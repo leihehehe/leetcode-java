7674daae0456885a9b67605fe7d04fa22001e9f5
LeiH <hslscarlett@gmail.com>
2023-08-10 23:57:13 +1000

    [415]Add Strings

--- src/doublePointer/[415]Add Strings.java
+++ src/doublePointer/[415]Add Strings.java
@@ -3,33 +3,19 @@ package doublePointer;
 //leetcode submit region begin(Prohibit modification and deletion)
 class AddStrings {
     public String addStrings(String num1, String num2) {
-        StringBuilder sb = new StringBuilder();
-        int i = num1.length()-1, j =num2.length()-1;
+        int p = num1.length()-1;
+        int q = num2.length()-1;
         int carry = 0;
-        while(i>=0 && j>=0){
-            int res = (num1.charAt(i)-'0')+(num2.charAt(j)-'0') + carry;
-            int digit = res%10;
-            carry = res/10;
+        StringBuilder sb = new StringBuilder();
+        while(p>=0 || q>=0 || carry!=0){
+            int a = p>=0? num1.charAt(p)-'0':0;
+            int b = q>=0? num2.charAt(q)-'0':0;
+            int result = a+b+carry;
+            int digit = result%10;
+            carry = result/10;
             sb.append(digit);
-            i--;
-            j--;
-        }
-        while(i>=0){
-            int res = num1.charAt(i)-'0'+carry;
-            int digit = res%10;
-            carry = res/10;
-            sb.append(digit);
-            i--;
-        }
-        while(j>=0){
-            int res = num2.charAt(j)-'0'+carry;
-            int digit = res%10;
-            carry = res/10;
-            sb.append(digit);
-            j--;
-        }
-        if(carry!=0){
-            sb.append(carry);
+            p--;
+            q--;
         }
         return sb.reverse().toString();
     }
