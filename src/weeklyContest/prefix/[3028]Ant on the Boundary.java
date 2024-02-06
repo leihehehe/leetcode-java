38e1d4a5b74b47fcb1a11dd973cf8cc6a83356fb
LeiH <hslscarlett@gmail.com>
2024-02-07 01:01:25 +0800

    Weekly Contest -> Week 383: Leetcode 3030 and Leetcode 3028

---
+++ src/weeklyContest/prefix/[3028]Ant on the Boundary.java
@@ -0,0 +1,22 @@
+package weeklyContest.prefix;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Easy
+ */
+class AntOnTheBoundary {
+    public int returnToBoundaryCount(int[] nums) {
+        int sum = 0;
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum==0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
