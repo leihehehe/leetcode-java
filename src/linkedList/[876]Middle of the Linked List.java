a4503a375bc05358e351dabe855a98e509d3bc35
LeiH <hslscarlett@gmail.com>
2024-02-06 21:26:34 +0800

    update [876]Middle of the Linked List

--- src/linkedList/[876]Middle of the Linked List.java
+++ src/linkedList/[876]Middle of the Linked List.java
@@ -3,7 +3,7 @@ package linkedList;
 /***
  * Leetcode 876
  */
-public class MiddleOfTheLinkedList {
+class MiddleOfTheLinkedList {
     public ListNode middleNode(ListNode head) {
         ListNode quick = head;
         ListNode slow = head;
@@ -14,6 +14,17 @@ public class MiddleOfTheLinkedList {
         return slow;
     }
     public ListNode middleNodeBefore(ListNode head) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode quick = dummyNode;
+        ListNode slow = dummyNode;
+        while(quick!=null && quick.next!=null){
+            slow = slow.next;
+            quick=quick.next.next;
+        }
+        return slow;
+    }
+    public ListNode middleNodeBefore2(ListNode head) {
         ListNode quick = head;
         ListNode slow = head;
         while(quick!=null && quick.next!=null&&quick.next.next!=null){
