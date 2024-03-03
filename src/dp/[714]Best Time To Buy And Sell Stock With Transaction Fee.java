aa4ae324dd938ee9173b1ac9c830aef3f3dc6fe2
LeiH <hslscarlett@gmail.com>
2024-03-04 10:59:48 +1100

    [714]Best Time To Buy And Sell Stock With Transaction Fee

--- src/dp/[714]Best Time To Buy And Sell Stock With Transaction Fee.java
+++ src/dp/[714]Best Time To Buy And Sell Stock With Transaction Fee.java
@@ -1,81 +1,57 @@
 package dp;
 
+import java.util.Arrays;
+
 /**
  * Leetcode 714
  */
-class BestTimeToBuyAndSellStockWithTransactionFee {
-    public int maxProfitMethod1V1(int[] prices, int fee) {
+class BestTimeToBuyAndSellStockWithTransactionSolution1 {
+    int[][] dp;
+    public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
+        dp = new int[n][2];
+        for(int[] d : dp) Arrays.fill(d,-1);
+        return dfs(prices, n-1, 0, fee);
+    }
+
+    public int dfs(int[] prices, int i, int hold, int fee){
+        if(i<0){
+            return hold==1?Integer.MIN_VALUE/2:0;
+        }
+        if(dp[i][hold]!=-1){
+            return dp[i][hold];
+        }
+        if(hold==1){
+            return dp[i][hold] = Math.max(dfs(prices,i-1,1,fee),dfs(prices,i-1,0,fee)-prices[i]);
+        }
+        return dp[i][hold] = Math.max(dfs(prices,i-1,0,fee),dfs(prices,i-1,1,fee)+prices[i]-fee);
+    }
+}
+class BestTimeToBuyAndSellStockWithTransactionSolution2 {
+    int[][] dp;
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        dp = new int[n+1][2];
+        dp[0][0] = 0;
+        dp[0][1] = Integer.MIN_VALUE/2;
         for(int i = 0;i<n;i++){
-            if(i==0){
-                //the first day, we don't hold a stock
-                dp[i][0] = 0;
-                dp[i][1] = -prices[i]-fee;
-            }else{
-                //sold stock(yesteday we held a stock)
-                dp[i][0] = Math.max(dp[i-1][1]+prices[i],dp[i-1][0]);
-                dp[i][1] = Math.max(dp[i-1][0]-prices[i]-fee, dp[i-1][1]);
-            }
+            dp[i+1][1] = Math.max(dp[i][1],dp[i][0]-prices[i]);
+            dp[i+1][0] = Math.max(dp[i][0],dp[i][1]+prices[i]-fee);
         }
-        return dp[n-1][0];
+        return dp[n][0];
     }
-    /**
-     * Method 1: dynamic programming
-     * @param prices
-     * @param fee
-     * @return
-     */
-    public int maxProfitMethod1V2(int[] prices, int fee) {
+}
+class BestTimeToBuyAndSellStockWithTransactionSolution3 {
+    int[] dp;
+    public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp=new int[n][2];
-        dp[0][1]=-prices[0]-fee;
-        for(int i = 1;i<n;i++){
-            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]+prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]-prices[i]-fee);
-        }
-        return dp[n-1][0];
-    }
-
-
-    /**
-     * Method 2: Optimized method to reduce space complexity
-     * @param prices
-     * @param fee
-     * @return
-     */
-    public int maxProfitMethod2 (int[] prices, int fee) {
-        int n = prices.length;
-        // int[][] dp=new int[n][2];
-        int[] dp = new int[2];
-        dp[1]=-prices[0]-fee;
-        for(int i = 1;i<n;i++){
-            dp[0] = Math.max(dp[0],dp[1]+prices[i]);
-            dp[1] = Math.max(dp[1],dp[0]-prices[i]-fee);
+        dp = new int[2];
+        dp[0] = 0;
+        dp[1] = Integer.MIN_VALUE/2;
+        for(int i = 0;i<n;i++){
+            dp[1] = Math.max(dp[1],dp[0]-prices[i]);
+            dp[0] = Math.max(dp[0],dp[1]+prices[i]-fee);
         }
         return dp[0];
-    }
-
-    /**
-     * Optimized
-     * @param prices
-     * @param fee
-     * @return
-     */
-    public int maxProfitMethod3(int[] prices, int fee) {
-        int n = prices.length;
-        int[][] dp = new int[2][2];
-        for(int i = 0;i<n;i++){
-            if(i==0){
-                //the first day, we don't hold a stock
-                dp[i&1][0] = 0;
-                dp[i&1][1] = -prices[i]-fee;
-            }else{
-                //sold stock(yesteday we held a stock)
-                dp[i&1][0] = Math.max(dp[(i-1)&1][1]+prices[i],dp[(i-1)&1][0]);
-                dp[i&1][1] = Math.max(dp[(i-1)&1][0]-prices[i]-fee, dp[(i-1)&1][1]);
-            }
-        }
-        return dp[(n-1)&1][0];
     }
 }
