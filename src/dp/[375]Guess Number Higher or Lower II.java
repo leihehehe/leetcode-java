b955d11503a19c920af31d92b49362788e5f7bb5
LeiH <hslscarlett@gmail.com>
2024-03-06 17:47:01 +1100

    [375]Guess Number Higher or Lower II

--- src/dp/[375]Guess Number Higher or Lower II.java
+++ src/dp/[375]Guess Number Higher or Lower II.java
@@ -1,27 +1,53 @@
 package dp;
 
+import java.util.Arrays;
+
 //leetcode submit region begin(Prohibit modification and deletion)
-class GuessNumberHigherOrLowerII {
-    //在class-level初始化的话，test cases公用一个cache，会很快
-    static int[][] cache = new int[201][201];
+class GetMoneyAmountSolution1 {
+    int[][] dp;
     public int getMoneyAmount(int n) {
+        dp = new int[n+1][n+1];
+        for(int i = 0;i<n;i++) Arrays.fill(dp[i], -1);
         return dfs(1,n);
     }
-    /*
-     * the minimum money I need to successfully guess the number within [l,r]
-     */
+
     public int dfs(int l, int r){
-        if(l>=r) return 0;
-        if(cache[l][r]>0) return cache[l][r];
-        int min  = Integer.MAX_VALUE;
-        for(int i = l;i<=r;i++){
-            //最坏情况下的成本，因为题目中说了要guaratee
-            //要么猜高了，要么猜底了，选最坏情况（成本最高的）
-            int cur = Math.max(dfs(l,i-1),dfs(i+1,r))+i;
-            min = Math.min(cur,min);
+        if(l>r){
+            return Integer.MAX_VALUE;
         }
-        cache[l][r] = min;
-        return min;
+        if(l==r)
+            return 0;
+        if(dp[l][r]!=-1) return dp[l][r];
+        int amount = Integer.MAX_VALUE;
+        for(int i = l;i<=r;i++){
+            amount = Math.min(amount, Math.max(dfs(l,i-1)+i,dfs(i+1,r)+i));
+        }
+        return dp[l][r] = amount;
     }
+}
+
+class GetMoneyAmountSolution2 {
+    int[][] dp;
+    public int getMoneyAmount(int n) {
+        dp = new int[n+2][n+1];
+        for(int l = n-1;l>=1;l--){
+            for(int r = l;r<=n;r++){
+                if(l==r){
+                    dp[l][r] = 0;
+                }else if(l>r){
+                    dp[l][r] = Integer.MAX_VALUE;
+                }else{
+                    int amount = Integer.MAX_VALUE;
+                    for(int i = l;i<=r;i++){
+                        amount = Math.min(amount, Math.max(dp[l][i-1]+i,dp[i+1][r]+i));
+                    }
+                    dp[l][r] = amount;
+                }
+
+            }
+        }
+        return dp[1][n];
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
