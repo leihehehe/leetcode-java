b8b25f26d4674727bde44376d2b5875eea265260
LeiH <hslscarlett@gmail.com>
2024-03-27 22:55:39 +1100

    [687]Longest Univalue Path

--- src/binaryTree/[687]Longest Univalue Path.java
+++ src/binaryTree/[687]Longest Univalue Path.java
@@ -1,6 +1,10 @@
 package binaryTree;
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
+ * Tree DP
+ */
+
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -16,26 +20,25 @@ package binaryTree;
  * }
  */
 class LongestUnivaluePath {
-    int maxLen = 0;
+    int res = 0;
     public int longestUnivaluePath(TreeNode root) {
-        getPathLen(root);
-        return maxLen;
+        dfs(root);
+        return res;
     }
-
-    public int getPathLen(TreeNode root){
-        if(root==null) return 0;
-        int leftLen = getPathLen(root.left);
-        int rightLen = getPathLen(root.right);
-        int curLen = 0;
-        if(root.left!=null && root.left.val == root.val)
-            curLen=leftLen+1;
-        if(root.right!=null && root.right.val == root.val)
-            curLen=Math.max(curLen,rightLen+1);
-        maxLen = Math.max(curLen,maxLen);
-        if(root.left!=null && root.right!=null && root.left.val == root.val && root.right.val == root.val){
-            maxLen = Math.max(maxLen,leftLen+rightLen+2);
+    //满足条件的单边最大path
+    public int dfs(TreeNode root){
+        if(root==null) return -1;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        //说明没有path可选，那么left就应该是-1（而不是0），因为这样加上当前节点，才能得到path为0
+        if(root.left!=null && root.left.val != root.val){
+            left = -1;
         }
-        return curLen;
+        if(root.right!=null && root.right.val!=root.val){
+            right = -1;
+        }
+        res = Math.max(left+right+2,res);
+        return Math.max(left,right)+1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
