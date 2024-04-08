1261f3e12e948b15f13613dddd346a91608dd4bd
LeiH <hslscarlett@gmail.com>
2024-04-08 12:02:30 +1000

    [1446]Consecutive Characters

--- src/Test.java
+++ src/Test.java
@@ -2,19 +2,81 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> ancestor = new ArrayList<>();
+        getSmallestString("xaxcd",4);
 
-        ancestor.add(4);
-        ancestor.add(3);
-        ancestor.add(5);
-        int len = 3;
-        for(int j = 0;j<len;j++)
-            ancestor.remove(ancestor.size()-1);
     }
-//[0,1,0] => 3
-    //[0],[1],[0],[0,1],[1,0],[0,1,0] => 6
-    //[0,1] => [0],[1],[0,1] => 2+1
-    //3+2+1
+
+    public static String getSmallestString(String s, int k) {
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length && k > 0; i++) {
+            int distance = chars[i] - 'a';
+            // 字符到'a'的直接距离和循环距离
+            int directDistance = distance;
+            int loopDistance = 26 - distance;
+
+            // 选择最小距离
+            int minDistance = Math.min(directDistance, loopDistance);
+
+            // 如果剩余的k足够减小距离
+            if (k >= minDistance) {
+                chars[i] = 'a'; // 将当前字符变为'a'
+                k -= minDistance; // 更新剩余的k
+            } else {
+                // 距离不够，只能减少部分距离
+                if (directDistance < loopDistance) {
+                    chars[i] -= k;
+                } else {
+                    chars[i] += k;
+                }
+                k = 0; // k已经用完
+            }
+        }
+
+        return new String(chars);
+    }
+    StringBuilder sb = new StringBuilder();
+    String res;
+    public void dfs(int i, String s, int k, int sum){
+        if(i==s.length()){
+            if(sum <= k && res.compareTo(sb.toString())>0)
+                res = sb.toString();
+            return;
+        }
+        char c = s.charAt(i);
+        //keep or not keep
+        sb.append(c);
+        dfs(i+1,s,k,sum);
+        sb.deleteCharAt(sb.length()-1);
+        //not keep, choose other letters
+        for(int j = 0;j<=25;j++){
+            int curSum = Math.min(Math.abs(c-'a'-j),Math.abs(c-'a'+26-j));
+            if(j==c-'a') continue;
+            if(sum+curSum>k) continue;
+            sb.append(j);
+            dfs(i+1,s,k,sum+curSum);
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+    }
+public static int longestMonotonicSubarray(int[] nums) {
+    int n = nums.length;
+    int[] dp = new int[n];
+    Arrays.fill(dp,1);
+    for(int i = 1;i<n;i++){
+        if(nums[i] == nums[i-1]+1){
+            dp[i] = dp[i-1]+1;
+        }else{
+            dp[i] = 1;
+        }
+
+    }
+    int max = 0;
+    for(int num:dp){
+        max = Integer.max(max,num);
+    }
+    return max;
+}
 public int minimumDistance(int[][] points) {
     List<Integer> t1 = new ArrayList<>();
     List<Integer> t2 = new ArrayList<>();
