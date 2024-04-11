2a0eaf21b90d4eca07ce32ecb85676fa32efd7bc
LeiH <hslscarlett@gmail.com>
2024-04-11 11:36:40 +1000

    [1839]Longest Substring Of All Vowels in Order

---
+++ src/weeklyContest/groupedIteration/[1839]Longest Substring Of All Vowels in Order.java
@@ -0,0 +1,31 @@
+package weeklyContest.groupedIteration;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class LongestBeautifulSubstring {
+    public int longestBeautifulSubstring(String word) {
+        char[] next = new char[26];
+        next[0] = 'e';
+        next['e'-'a'] = 'i';
+        next['i'-'a'] = 'o';
+        next['o'-'a'] = 'u';
+        int i = 0;
+        int n = word.length();
+        int max = 0;
+        while(i<n){
+            if(word.charAt(i)!='a'){
+                i++;
+                continue;
+            }
+            int start = i;
+            i++;
+            while(i<n && (word.charAt(i) == next[word.charAt(i-1)-'a'] || word.charAt(i) == word.charAt(i-1))){
+                i++;
+            }
+            if(word.charAt(i-1)=='u'){
+                max = Math.max(i-start,max);
+            }
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
