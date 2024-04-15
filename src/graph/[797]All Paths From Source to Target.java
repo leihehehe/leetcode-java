7ddc4e6a72a9d838e34e1c7320865d2a8dbaa84e
LeiH <hslscarlett@gmail.com>
2024-04-15 16:44:03 +1000

    [2316]Count Unreachable Pairs of Nodes in an Undirected Graph

--- src/graph/[797]All Paths From Source to Target.java
+++ src/graph/[797]All Paths From Source to Target.java
@@ -4,7 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class AllPathsSourceTarget {
+class AllPathsSourceTargetSolution1 {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(0,graph,path);
+
+        return res;
+    }
+    public void dfs(int i, int[][] graph,List<Integer> path){
+        if(i==graph.length-1){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        int[] neighbors = graph[i];
+        for(int neighbor:neighbors){
+            path.add(neighbor);
+            dfs(neighbor,graph,path);
+            path.remove(path.size()-1);
+        }
+    }
+}
+class AllPathsSourceTargetSolution2 {
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<Integer> path = new ArrayList<>();
