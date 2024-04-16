c1ccf9a0fc3412301d499eb8d0e9d1ea5a147ad0
LeiH <hslscarlett@gmail.com>
2024-04-16 12:10:39 +1000

    update 2192

--- src/graph/[2192]All Ancestors of a Node in a Directed Acyclic Graph.java
+++ src/graph/[2192]All Ancestors of a Node in a Directed Acyclic Graph.java
@@ -2,10 +2,11 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class GetAncestors {
+class GetAncestorsSolution1 {
     List<Integer>[] ans;
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
         List<Integer>[] g = new ArrayList[n];
@@ -35,6 +36,41 @@ class GetAncestors {
         }
     }
 
+}
+class GetAncestorsSolution2 {
+    List<Integer>[] res;
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        List<Integer>[] graph = new ArrayList[n];
+        Arrays.setAll(graph, x -> new ArrayList<>());
+        for(int[] edge:edges){
+            int to =edge[0];
+            int from = edge[1];
+            graph[from].add(to);
+        }
+        visited = new int[n];
+        Arrays.fill(visited,-1);
+        res = new ArrayList[n];
+        Arrays.setAll(res, x -> new ArrayList<>());
+        for(int i =0;i<n;i++){
+            dfs(i,graph,i);
+        }
+        for(List<Integer> r: res){
+            Collections.sort(r);
+        }
+
+        return Arrays.asList(res);
+
+    }
+    int[] visited;
+    public void dfs(int i, List<Integer>[] graph, int start){
+        for(int neighbor:graph[i]){
+            if(visited[neighbor]!=start){
+                visited[neighbor] = start;
+                res[start].add(neighbor);
+                dfs(neighbor,graph,start);
+            }
+        }
+    }
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
--- src/math/[447]Number of Boomerangs.java
+++ src/math/[447]Number of Boomerangs.java
@@ -12,7 +12,30 @@ class NumberOfBoomerangs {
         for(int i = 0;i<n-1;i++){
             int x1 = points[i][0];
             int y1 = points[i][1];
+            map.clear();
             for(int j = i+1;j<n;j++){
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                int distance = Math.abs(x1-x2)*Math.abs(x1-x2)+Math.abs(y1-y2)*Math.abs(y1-y2);
+                int count = map.getOrDefault(distance,0);
+                //边统计边计算当前point和之前的point
+                ans+=count*2;
+                map.put(distance,count+1);
+            }
+        }
+        return ans;
+    }
+}
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int n = points.length;
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            map.clear();
+            for(int j = 0;j<n;j++){
                 int x2 = points[j][0];
                 int y2 = points[j][1];
                 int distance = Math.abs(x1-x2)*Math.abs(x1-x2)+Math.abs(y1-y2)*Math.abs(y1-y2);
