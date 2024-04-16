38f36288af77cbafb01d368c8a55683a1feb971f
LeiH <hslscarlett@gmail.com>
2024-04-16 15:53:25 +1000

    [739]Daily Temperatures

--- src/stackAndDeque/[739]Daily Temperatures.java
+++ src/stackAndDeque/[739]Daily Temperatures.java
@@ -4,23 +4,37 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class DailyTemperatures {
+class DailyTemperaturesSolution1 {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] answer = new int[temperatures.length];
+        int n = temperatures.length;
+        int[] ans = new int[n];
         Deque<Integer> stack = new ArrayDeque<>();
-        for(int i = 0;i<temperatures.length;i++){
-            int cur = temperatures[i];
-            if(stack.isEmpty()){
-                stack.push(i);
-            }else{
-                while(!stack.isEmpty() && cur>temperatures[stack.peek()]){
-                    int index = stack.pop();
-                    answer[index] = i-index;
-                }
-                stack.push(i);
+        for(int i = 0;i<n;i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()]<temperatures[i]){
+                int top = stack.pop();
+                ans[top] = i-top;
             }
+            stack.push(i);
         }
-        return answer;
+        return ans;
+    }
+}
+class DailyTemperaturesSolution2 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+        //[73,74,75,71,69,72,76,73]
+        for(int i = n-1;i>=0;i--){
+            while(!stack.isEmpty() && temperatures[stack.peek()]<=temperatures[i]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                ans[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
