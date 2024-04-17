0f3908d26758e576eee8c0f7f075f62c404a2ab2
LeiH <hslscarlett@gmail.com>
2024-04-17 14:59:05 +1000

    [42]Trapping Rain Water

--- src/stackAndDeque/[42]Trapping Rain Water.java
+++ src/stackAndDeque/[42]Trapping Rain Water.java
@@ -4,29 +4,67 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class TrappingRainWater {
+class TrapSolution1 {
     public int trap(int[] height) {
-        //stack storing indices
-        Deque<Integer> stack = new ArrayDeque<>();
-        int sum = 0;
-        for(int i =0;i<height.length;i++){
-            while(!stack.isEmpty() && height[stack.peek()] <height[i]){
-                //stack.size() == 1?
-                int midIndex = stack.pop();
-                int midHeight = height[midIndex];
-                if(!stack.isEmpty()){
-                    int leftIndex = stack.peek();
-                    int waterHeight = Math.min(height[leftIndex],height[i])-midHeight;
-                    int waterWidth = i-leftIndex-1;
-                    sum+=waterHeight*waterWidth;
-                }
+        //前后缀
+        int n = height.length;
+        int[] preMax = new int[n];
+        int[] postMax = new int[n];
+        preMax[0] = height[0];
+        postMax[n-1] = height[n-1];
+        for(int i = 1,j=n-2;i<n & j>=0;i++,j--){
+            preMax[i] =  Math.max(preMax[i-1],height[i]);
+            postMax[j] = Math.max(postMax[j+1],height[j]);
+        }
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            ans+= Math.min(preMax[i], postMax[i])-height[i];
+        }
+        return ans;
 
+    }
+}
+class TrapSolution2 {
+    public int trap(int[] height) {
+        //前后缀
+        int n = height.length;
+        int preMax= 0, sufMax = 0;
+        int l = 0, r = n-1;
+        int ans = 0;
+        while(l<r){
+            preMax = Math.max(preMax,height[l]);
+            sufMax = Math.max(sufMax,height[r]);
+            if(preMax<sufMax){
+                ans+=preMax-height[l];
+                l++;
+            }else{
+                ans+=sufMax - height[r];
+                r--;
+            }
+        }
+        return ans;
+
+    }
+}
+class TrapSolution3 {
+    public int trap(int[] height) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int n = height.length;
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            while(!stack.isEmpty()&& height[stack.peek()]<height[i]){
+                int midIndex = stack.pop();
+                if(stack.isEmpty()){
+                    break;
+                }
+                int leftIndex = stack.peek();
+                int width = i-leftIndex-1;
+                ans+=width*(Math.min(height[leftIndex],height[i]) - height[midIndex]);
             }
             stack.push(i);
         }
-        return sum;
+        return ans;
     }
-
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
