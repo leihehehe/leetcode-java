b7e9a55590f3b1f1d9c7058d9504ab4543c5398f
LeiH <hslscarlett@gmail.com>
2024-04-19 01:27:12 +1000

    [2007]Find Original Array From Doubled Array

--- src/Test.java
+++ src/Test.java
@@ -5,106 +5,38 @@ public class Test {
 //        String s = "[[1,0,0,0,0,0,0,0,1],[0,1,0,1,0,0,0,0,0],[0,0,1,1,0,1,0,0,0],[0,1,1,1,1,0,1,0,0],[0,0,0,1,1,1,0,0,0],[0,0,1,0,1,1,0,0,0],[0,0,0,1,0,0,1,1,0],[0,0,0,0,0,0,1,1,1],[1,0,0,0,0,0,0,1,1]]\n";
 //        s = s.replace('[','{').replace(']','}');
 //        System.out.println(s);
-        Test test = new Test();
-        int[][] graph = new int[][]{{1,0,0,0,0,0,0,0,1},{0,1,0,1,0,0,0,0,0},{0,0,1,1,0,1,0,0,0},{0,1,1,1,1,0,1,0,0},{0,0,0,1,1,1,0,0,0},{0,0,1,0,1,1,0,0,0},{0,0,0,1,0,0,1,1,0},{0,0,0,0,0,0,1,1,1},{1,0,0,0,0,0,0,1,1}};
-        System.out.println(test.minMalwareSpread(graph, new int[]{3, 7}));
-    }
-
-    public int minMalwareSpread(int[][] graph, int[] initial) {
-        int n = graph.length;
-        boolean[] isInfected = new boolean[n];
-        for (int init : initial) {
-            isInfected[init] = true;
-        }
-        UnionFind uf = new UnionFind(n);
-        //得到没有被感染的连通块
-        for (int i = 0; i < n; i++) {
-            if (isInfected[i]) continue;
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                if (graph[i][j] == 1 && !isInfected[j] && !uf.isConnected(i, j)) {
-                    uf.union(i, j);
-                }
-            }
-        }
-        int maxNotInfectedNodes = Integer.MIN_VALUE;
-        int minIndexNode = n;
-        boolean[] visited = new boolean[n];
-        //key: 没有被感染的node(root), value:被感染node的邻居个数
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int infected : initial) {
-            boolean[] counted = new boolean[n];
-            for (int neighbor = 0; neighbor < n; neighbor++) {
-                if (neighbor == infected || graph[infected][neighbor] == 0 || isInfected[neighbor]) continue;
-                int root = uf.find(neighbor);
-                if(!counted[root]){
-                    counted[root] = true;
-                    countMap.put(root, countMap.getOrDefault(root, 0) + 1);
-                }
-            }
-        }
-        for (int infected : initial) {
-            int notInfectedNodes = 0;
-            //避免重复统计同一group的数量
-            Arrays.fill(visited, false);
-            for (int neighbor = 0; neighbor < n; neighbor++) {
-                if (neighbor == infected || graph[infected][neighbor] == 0 || isInfected[neighbor]) continue;
-                int root = uf.find(neighbor);
-                if (!visited[root]) {
-                    visited[root] = true;
-                    if (countMap.get(root) == 1)
-                        notInfectedNodes += uf.getSize(root);
-                }
-            }
-            if (notInfectedNodes > maxNotInfectedNodes) {
-                maxNotInfectedNodes = notInfectedNodes;
-                minIndexNode = infected;
-            } else if (notInfectedNodes == maxNotInfectedNodes && infected < minIndexNode) {
-                minIndexNode = infected;
-            }
-        }
-        return minIndexNode;
+        Solution1 solution = new Solution1();
+        solution.findOriginalArray(new int[]{3,0});
 
     }
 
-    class UnionFind {
-        int[] count;
-        int[] parent;
+}
+class Solution1 {
+    public int[] findOriginalArray(int[] changed) {
+        int n = changed.length;
+        Arrays.sort(changed);
+        Map<Integer,Integer> map = new HashMap<>();
 
-        public UnionFind(int n) {
-            this.count = new int[n];
-            this.parent = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
+        for(int i = 0;i<n;i++){
+            map.put(changed[i],map.getOrDefault(changed[i], 0)+1);
+        }
+        int count = n/2;
+        int i = n-1, j = 0;
+        int[] res = new int[n/2];
+        while(i>=0 && count>0){
+            if(changed[i]%2==0 && map.get(changed[i])>0 && map.get(changed[i]/2)>0){
+                map.put(changed[i],map.getOrDefault(changed[i],0)-1);
+                map.put(changed[i]/2,map.getOrDefault(changed[i]/2,0)-1);
+                res[j++] = changed[i]/2;
+                count--;
             }
-            Arrays.fill(count, 1);
+            i--;
         }
-
-        public boolean isConnected(int i, int j) {
-            return find(i) == find(j);
-        }
-
-        public int find(int i) {
-            if (parent[i] != i) {
-                parent[i] = find(parent[i]);
-            }
-            return parent[i];
-        }
-
-        public void union(int i, int j) {
-            int rootI = find(i);
-            int rootJ = find(j);
-            if (rootI != rootJ) {
-                parent[rootI] = rootJ;
-                count[rootJ] += count[rootI];
-            }
-
-        }
-
-        public int getSize(int i) {
-            return count[i];
-        }
+        return count==0?res:new int[]{};
+        //4,8,4
+        //2,4,2
+        //2,4,2,4,8,4
+        //2,2,4,4,4,8
 
     }
-
 }
---
+++ src/weeklyContest/graph/[2007]Find Original Array From Doubled Array.java
@@ -0,0 +1,29 @@
+package weeklyContest.graph;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class FindOriginalArray {
+    public int[] findOriginalArray(int[] changed) {
+        int n = changed.length;
+        Arrays.sort(changed);
+        Map<Integer,Integer> count = new HashMap<>();
+        int[] res = new int[n/2];
+        //一次遍历
+        for(int i = 0,j = 0;i<n;i++){
+            if(!count.containsKey(changed[i])){
+                if(j==n/2) return new int[]{};
+                //这里不需要添加changed[i]，因为后面不可能需要changed[i]了
+                count.put(changed[i]*2, count.getOrDefault(changed[i]*2,0)+1);
+                res[j++] = changed[i];
+            }else{
+                count.put(changed[i], count.get(changed[i])-1);
+                if(count.get(changed[i])==0) count.remove(changed[i]);
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
