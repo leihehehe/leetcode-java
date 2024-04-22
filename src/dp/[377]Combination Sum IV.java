9db4610b33a8c3f2c1348e150aa7fc58885896ba
LeiH <hslscarlett@gmail.com>
2024-04-22 21:33:51 +1000

    [377]Combination Sum IV

--- src/dp/[377]Combination Sum IV.java
+++ src/dp/[377]Combination Sum IV.java
@@ -1,29 +1,41 @@
 package dp;
 
+import java.util.Arrays;
+
 /**
  * Leetcode 377
  */
-class CombinationSumIV {
-    /**
-     * TIME: O(N*target)
-     * SPACE: O(target)
-     * @param nums
-     * @param target
-     * @return
-     */
+class CombinationSum4Solution1 {
     public int combinationSum4(int[] nums, int target) {
-        // 1 2 3, target is 4
-        // 1 + 3
-        // 2 + 2
-        // 3 + 1
-        int[] dp = new int[target+1];
-        dp[0]=1;
-        for(int i = 1; i <target+1; i++){
-            for(int num : nums){
-                if(i>=num)
-                    dp[i] +=dp[i-num];
-            }
+        memo = new int[target+1];
+        Arrays.fill(memo,-1);
+        return dfs(nums,target);
+    }
+    int[] memo;
+    public int dfs(int[] nums, int target){
+        if(target==0) return 1;
+        if(target<0) return 0;
+        if(memo[target]!=-1) return memo[target];
+        int res = 0;
+        for(int num:nums){
+            if(target<num) continue;
+            res+=dfs(nums,target-num);
         }
-        return dp[target];
+        return memo[target] = res;
+    }
+}
+class CombinationSum4Solution2 {
+    public int combinationSum4(int[] nums, int target) {
+        int[] memo = new int[target+1];
+        memo[0] = 1;
+        for(int i = 1;i<=target;i++){
+            int res = 0;
+            for(int num:nums){
+                if(i<num) continue;
+                res+=memo[i-num];
+            }
+            memo[i] = res;
+        }
+        return memo[target];
     }
 }
