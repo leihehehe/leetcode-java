afe1e722a133013e271ad4523dd3a1b3115a3920
LeiH <hslscarlett@gmail.com>
2024-07-28 16:51:45 +1000

    [523]Continuous Subarray Sum

--- src/prefixSumAndDifference/[523]Continuous Subarray Sum.java
+++ src/prefixSumAndDifference/[523]Continuous Subarray Sum.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  * Leetcode 523
  */
-public class ContinuousSubarraySum {
+class ContinuousSubarraySumSolution1 {
     public boolean checkSubarraySum(int[] nums, int k) {
         //find a interval res[] -> sum%k=0
         int[] preSum = new int[nums.length+1];
@@ -27,6 +27,26 @@ public class ContinuousSubarraySum {
             if(map.containsKey(need)){
                 if(i-map.get(need)>=2) return true;
             }
+        }
+        return false;
+    }
+}
+class ContinuousSubarraySumSolution2 {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        //(x-y)%k = 0
+        //x%k == y%k
+        int sum = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 1;i<nums.length;i++){
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+            sum = (sum+nums[i-1])%k;
+            if(sum<0) sum+=k;
+            int curSum = (sum+nums[i])%k;
+            if(curSum<0) curSum+=k;
+            if(map.getOrDefault(curSum,0)>0){
+                return true;
+            }
+
         }
         return false;
     }
