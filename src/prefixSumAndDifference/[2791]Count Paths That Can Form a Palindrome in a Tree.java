c9b366dd17400cba22e7074bd339679a9abff399
LeiH <hslscarlett@gmail.com>
2024-08-08 13:06:35 +1000

    [2791]Count Paths That Can Form a Palindrome in a Tree

--- src/prefixSumAndDifference/[2791]Count Paths That Can Form a Palindrome in a Tree.java
+++ src/prefixSumAndDifference/[2791]Count Paths That Can Form a Palindrome in a Tree.java
@@ -2,7 +2,39 @@ package prefixSumAndDifference;
 
 import java.util.*;
 
-class CountPalindromePathsSolution {
+class CountPalindromePathsSolution1 {
+    Map<Integer, Integer> map = new HashMap<>();
+    private long res;
+
+    public long countPalindromePaths(List<Integer> parent, String s) {
+        int n = s.length();
+        List<int[]>[] graph = new ArrayList[n];
+        Arrays.setAll(graph, e -> new ArrayList<>());
+
+        for (int i = 1; i < parent.size(); i++) {
+            int father = parent.get(i);
+            char c = s.charAt(i);
+            graph[father].add(new int[]{i, 1 << (c - 'a')});
+        }
+
+        dfs(0, 0, graph);
+        return res;
+    }
+
+    private void dfs(int x, int xor,List<int[]>[] graph) {
+        res += map.getOrDefault(xor, 0);
+        for (int i = 0; i < 26; ++i) {
+            res += map.getOrDefault(xor ^ (1 << i), 0);
+        }
+        map.merge(xor, 1, Integer::sum);
+        for (int[] neighbor : graph[x]) {
+            int y = neighbor[0];
+            int m = neighbor[1];
+            dfs(y, xor ^ m,graph);
+        }
+    }
+}
+class CountPalindromePathsSolution2 {
     Map<Integer,Integer> map = new HashMap<>();
     public long countPalindromePaths(List<Integer> parent, String s) {
         //palindrome -> frequency -> even -> odd is 0
